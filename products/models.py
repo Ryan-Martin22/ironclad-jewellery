4e98a3c6bdb6323bb37aac3f7603937bab54bb26
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-05-03 16:19:39 +0000

    add categories and product model

--- products/models.py
+++ products/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
+    
+    class Meta:
+        """Removes extra 's' from Model name"""
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -10,3 +15,22 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Products(models.Model):
+    """Products model"""
+    class Meta:
+        """Removes extra 's' from Model name"""
+        verbose_name_plural = 'Products'
+
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
