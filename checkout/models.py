c3893679ad2d2e83d9b8283aa144ef587681519c
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-05-09 15:43:41 +0000

    add model methods to order line item model

--- checkout/models.py
+++ checkout/models.py
@@ -61,3 +61,14 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
