ccaf543592c602ee950928cd23c2eab7925f1a86
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-05-11 13:57:23 +0000

    add edit to webhooks to fix error

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,17 +1,18 @@
+"""Listens for Stripe Webhooks
+Code from Stripe and Boutique Ado Follow Along Project"""
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-
+import stripe
 from checkout.webhook_handler import StripeWH_Handler
 
-import stripe
 
 @require_POST
 @csrf_exempt
 def webhook(request):
     """Listen for webhooks from Stripe"""
-    # Setup
+    # Setup api key and secret
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -22,7 +23,7 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
+            payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
@@ -39,7 +40,8 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed':
+            handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
