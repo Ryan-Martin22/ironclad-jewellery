8de6f1483f5bd86049667e5bfbede90393448076
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-05-17 11:52:38 +0000

    add review to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -19,7 +19,19 @@ class CategoryAdmin(admin.ModelAdmin):
         'friendly_name',
         'name',
     )
+    
+    
+class ReviewAdmin(admin.ModelAdmin):
+    """Admin Panel display for Product Model"""
+    list_display = (
+        'product',
+        'user',
+        'rating',
+        'review',
+        'status',
+    )
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)
