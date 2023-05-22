b1d229b8ca3534a6f1b3acc1e112a22f71d966d1
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-05-22 11:35:54 +0000

    add footer and about us page

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.aboutus, name='about_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,10 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def aboutus(request):
+    """
+    A view to return about us page
+    """
+    return render(request, 'about/aboutus.html')
