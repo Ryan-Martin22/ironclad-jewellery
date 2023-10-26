f40e6ac1e6e1e2df2d70d0524faddb9ab18f5e56
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-10-26 14:06:29 +0000

    add validation fix to bag views.py

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    HttpResponse,
+    get_object_or_404,
+    )
+
 from django.contrib import messages
 
 from products.models import Product
@@ -28,7 +35,8 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
