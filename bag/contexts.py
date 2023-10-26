eaf57b023ca9609325daa594893e4a015447f25c
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-10-26 13:56:42 +0000

    add whitespace removal,apps.py for validation

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,7 +10,7 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-    
+
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
@@ -27,9 +27,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
